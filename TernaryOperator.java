189fbfb9ea5577bae7e64891dd1de03af07c8734
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-01-29 00:45:31 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+class TernaryOperator{
+
+    public static void main(String args[]){
+
+        int num = 14;
+        int result = 0;        
+
+        result = num%2==0?10:20;
+
+        System.out.println(result);
+    }
+}
