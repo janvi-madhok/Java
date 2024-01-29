f853c631bfab49c4e9a7568e2eec63b78ec73bec
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-01-29 23:39:25 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,16 @@
+class DoWhileLoop{
+
+    public static void main(String args[]){
+
+        int hiCount = 1;
+
+        do{
+
+            System.out.println("Hi, This is me.");
+
+            hiCount++;
+
+        }while(hiCount<5);
+
+    }
+}
