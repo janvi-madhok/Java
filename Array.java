dd1453d6e8e3b63eb4b4b1350a656cbf8f4e18dc
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-04 00:23:47 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,26 @@
+public class Array{
+
+    public static void main(String args[]){
+
+        int num[] = {2,3,1,5,8};
+
+        for(int i=0;i<5;i++){
+            System.out.print(num[i]+" ");
+        }
+
+        System.out.println();
+
+        int arr[] = new int[3];
+
+        arr[0] = 12;
+        arr[1] = 23;
+        arr[2] = 40;
+
+        for(int j=0;j<3;j++){
+            System.out.print(arr[j]+" ");
+        }
+
+
+
+    }
+}
