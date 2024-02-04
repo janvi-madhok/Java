a621a6a765a0b33e0980c0f9827081e7e4792284
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-04 15:32:18 +0530

    Add files via upload

---
+++ ArrayOfObjects.java
@@ -0,0 +1,40 @@
+class Employee{
+
+    int empId;
+    String name;
+    double salary;
+
+}
+
+public class ArrayOfObjects{
+
+    public static void main(String args[]){
+
+        Employee e1 = new Employee();
+        e1.empId = 234;
+        e1.name = "Shaan";
+        e1.salary = 345000;
+
+        Employee e2 = new Employee();
+        e2.empId = 346;
+        e2.name = "Neha";
+        e2.salary = 456430;
+
+        Employee e3 = new Employee();
+        e3.empId = 346;
+        e3.name = "Arijit";
+        e3.salary = 245000;
+
+       
+        Employee emp[] = new Employee[3]; 
+        emp[0] = e1;
+        emp[1] = e2;
+        emp[2] = e3;
+
+        for(int i=0;i<emp.length;i++){
+            System.out.println(emp[i].empId+" - "+emp[i].name+" : "+emp[i].salary);
+        }
+
+
+    }
+}
