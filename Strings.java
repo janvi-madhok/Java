e6f70c62fceade70690c3bac6f7edbb78683b11c
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-04 17:19:03 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,15 @@
+public class Strings{
+    
+    public static void main(String args[]){
+
+        String str = new String("HelloWorld");
+
+        System.out.println(str);
+        System.out.println(str.charAt(6));
+        System.out.println(str.concat("India"));
+        System.out.println(str.hashCode());
+
+        String str2 = "Learn Programming";
+        System.out.println(str2);
+    }
+}
